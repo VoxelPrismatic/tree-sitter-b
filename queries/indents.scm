392f4b93408e59aad028b26b2b8979bc3624fa5d
VoxelPrismatic <voxelprismatic@pm.me>
2025-07-08 17:57:35 -0500

    indents?

--- queries/indents.scm
+++ queries/indents.scm
@@ -1,55 +1,28 @@
-([
-  (block_statement)
-  (while_statement)
-  (if_statement)
-] @indent.begin
+((block_statement) @indent.begin
   (#set! indent.immediate))
 
-; ((block_statement) @indent.begin
-;   (#set! indent.immediate))
-; ((while_statement) @indent.begin
-;   (#set! indent.immediate))
+((while_statement) @indent.begin
+  (#set! indent.immediate))
+
+((if_statement) @indent.begin
+  (#set! indent.immediate))
+
 (while_statement
   (statement
     ";" @indent.end @indent.branch))
 
 (while_statement
   (statement
-    (return_statement
+    (_
       ";" @indent.end @indent.branch)))
 
-(while_statement
-  (statement
-    (auto_statement
-      ";" @indent.end @indent.branch)))
-
-(while_statement
-  (statement
-    (extrn_statement
-      ";" @indent.end @indent.branch)))
-
-; ((if_statement) @indent.begin
-;   (#set! indent.immediate))
 (if_statement
   (statement
     ";" @indent.end @indent.branch))
 
 (if_statement
   (statement
-    (return_statement
+    (_
       ";" @indent.end @indent.branch)))
 
-(if_statement
-  (statement
-    (auto_statement
-      ";" @indent.end @indent.branch)))
-
-(if_statement
-  (statement
-    (extrn_statement
-      ";" @indent.end @indent.branch)))
-
-[
-  ")"
-  "}"
-] @indent.end @indent.branch
+"}" @indent.branch @indent.end
